36bc917319915b848d30bfccb61fd0408c1c8130
AlbertAlfaro <alber.cx@gmail.com>
2023-04-30 19:06:06 -0500

    remove datatable

--- src/main.js
+++ src/main.js
@@ -9,13 +9,11 @@ import './assets/main.css'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
-import DataTable from 'datatables.net-vue3'
 
 const app = createApp(App);
 app.component('ErrorAlert', ErrorAlert);
 app.component('SuccessAlert', SuccessAlert);
 app.component('heders', heders);
-app.component('DataTable', DataTable);
 app.use(Toast);
 app.use(router);
 app.mount('#app')
